10540741f590f6cee816b165809c64d5c7a93800
Shaan Nobee <sniper111@gmail.com>
2019-05-13 14:23:06 +0400

    Update eventstore.rb

--- Casks/eventstore.rb
+++ Casks/eventstore.rb
@@ -1,6 +1,6 @@
 cask 'eventstore' do
-  version '5.0.0'
-  sha256 '5bcea119d3971c3bf9de5e0ce2a6963818a508a7071909a3d8d2b44a6d9063ed'
+  version '5.0.1'
+  sha256 '517e920643371f6924cec558e92193d1a494a24020483c5f98acf264dbf0498c'
 
   url "https://eventstore.org/downloads/macos/EventStore-OSS-MacOS-macOS-v#{version}.tar.gz"
   name 'Event Store'
